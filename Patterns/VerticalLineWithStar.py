aff67579b84ec37140ad1f8d52f8d98dec143af3
dikshasharma29 <deehaans29@gmail.com>
2025-08-28 18:24:22 +0530

    VerticalLineWithStar.py

--- Patterns/VerticalLineWithStar.py
+++ Patterns/VerticalLineWithStar.py
@@ -1,4 +1,4 @@
-n=int(input("Enter a number:"))
+n=int(input("Enter no. of rows: "))
 for i in range(1,n+1):
     for j in range(1,n+1):
         if (j==1 or j==n):
@@ -6,3 +6,4 @@ for i in range(1,n+1):
         else:
             print("_",end="")
     print()
+
