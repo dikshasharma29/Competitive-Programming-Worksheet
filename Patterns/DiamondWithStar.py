4ad953d4f6371b6af438ce2b8882eabb51367f9c
dikshasharma29 <deehaans29@gmail.com>
2025-08-28 18:22:11 +0530

    DiamondWithStar.py

--- Patterns/DiamondWithStar.py
+++ Patterns/DiamondWithStar.py
@@ -1,4 +1,4 @@
-n = int(input("Enter rows: "))
+n = int(input("Enter no. of rows: "))
 
 for i in range(1, n+1):
     for j in range(i):
@@ -9,3 +9,4 @@ for i in range(n-1, 0, -1):
     for j in range(i):
         print("*", end="")
     print()
+
