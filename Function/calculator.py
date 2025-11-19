205e1e8c7d432acad0f720f6d1a09497baca630d
dikshasharma29 <deehaans29@gmail.com>
2025-11-19 21:35:05 +0530

    calculator.py

---
+++ Function/calculator.py
@@ -0,0 +1,30 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("1 Add")
+print("2 Subtract")
+print("3 Multiply")
+print("4 Divide")
+
+choice = int(input().strip())
+a, b = map(int, input().split())
+
+if choice == 1:
+    print(add(a, b))
+elif choice == 2:
+    print(sub(a, b))
+elif choice == 3:
+    print(mul(a, b))
+elif choice == 4:
+    print(div(a, b))
+else:
+    print("Invalid")
